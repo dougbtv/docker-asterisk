e1d15d482a51f7878dc01b746d184ac515732bbe
dougbtv <douglaskippsmith@gmail.com>
2014-10-16 20:39:47 -0400

    [autobuilder][examples] updates example script

--- tools/autobuilder/examples/run_docker.sh
+++ tools/autobuilder/examples/run_docker.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# It likes to complain about the port if you don't restart it.
+echo "Restarting docker..."
+systemctl restart docker
+# Make sure everything else is clear.
 echo "Kill all containers"
 docker kill $(docker ps -a -q)
 echo "Removing all containers"
 docker rm $(docker ps -a -q)
+
+# Start the recurive matrix of oddity.
 echo "Starting docker-in-docker..."
 docker run \
   --privileged \
@@ -11,3 +17,13 @@ docker run \
   -e PORT=4444 \
   -t jpetazzo/dind:latest
 echo "Starting autobuilder..."
+
+docker run \
+  --name autobuilder \
+  --link dind:dind \
+  -e PORT=4444 \
+  -t dougbtv/asterisk-autobuilder \
+  forever -e /var/log/autobuilder.log -o /var/log/autobuilder.log autobuilder.js \
+    -u asteriskautobuilder -p "aaaaaaaaaaaaaaaaa" \
+    --docker_user dougbtv --docker_password "aaaaaaaaaaaaaa" --docker_email="d00d@d00d.com" \
+    --skipclone --irc_debug --irc_disabled --forceupdate --authdisabled
