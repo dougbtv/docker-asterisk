a5744059b9e310ed3c005242926128197ec4a1a9
dougbtv <douglaskippsmith@gmail.com>
2014-12-05 08:03:57 -0500

    [interim] working on dockerfile for dispatch-watcher, not working yet

--- high-availability/etcd.sh
+++ high-availability/etcd.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-IP_ADDR="192.168.122.1"
+IP_ADDR="192.168.100.1"
 # $(ifconfig | grep -i 192.168.100 | awk '{print $2}')
 
 docker run --rm -it \
     -p 4001:4001 -p 7001:7001 \
     -v /var/etcd/:/data \
     -i -t coreos/etcd:latest \
-    -discovery=https://discovery.etcd.io/a9183c3c0f556f37bb9f10de85086fcc \
+    -discovery=https://discovery.etcd.io/c04ddddad4fbcc992391404268c6b3cf \
     -peer-addr $IP_ADDR:7001 \
     -addr $IP_ADDR:4001 \
     -name etcdindocker1234
