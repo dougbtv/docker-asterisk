4a69bfb882443c997244437730bf9a52bc47b872
dougbtv <douglaskippsmith@gmail.com>
2014-12-06 20:01:38 -0500

    [ha][kamailio] adds weighted as method over round-robin for dispatcher

--- high-availability/etcd.sh
+++ high-availability/etcd.sh
@@ -7,7 +7,7 @@ docker run --rm -it \
     -p 4001:4001 -p 7001:7001 \
     -v /var/etcd/:/data \
     -i -t coreos/etcd:latest \
-    -discovery=https://discovery.etcd.io/84b1b674bc96081e8060de7289feb5a8 \
+    -discovery=https://discovery.etcd.io/09f948f31d4deacfdc6f613e5c597700 \
     -peer-addr $IP_ADDR:7001 \
     -addr $IP_ADDR:4001 \
     -name etcdindocker1234
