fc56df1e914e8ebc7049a45b711145f5edf9397d
dougbtv <douglaskippsmith@gmail.com>
2014-12-08 22:58:36 -0500

    [ha][dispatcher][dockerfile] tweaks while testing running in docker

--- high-availability/dispatcher.sh
+++ high-availability/dispatcher.sh
@@ -4,5 +4,5 @@ IP_ADDRESS=$(ifconfig | grep -A 1 p4p1 | tail -n 1 | awk '{print $2}')
 
 docker run \
     --volumes-from kamailio \
-    -i -t dougbtv/dispatch-watcher \
+    -i -t dougbtv/kamailio-etcd-dispatcher \
     --etcdhost $IP_ADDRESS
--- high-availability/etcd.sh
+++ high-availability/etcd.sh
@@ -7,7 +7,7 @@ docker run --rm -it \
     -p 4001:4001 -p 7001:7001 \
     -v /var/etcd/:/data \
     -i -t coreos/etcd:latest \
-    -discovery=https://discovery.etcd.io/09f948f31d4deacfdc6f613e5c597700 \
+    -discovery=https://discovery.etcd.io/91d3fd52932813bef18b0db71f50457e \
     -peer-addr $IP_ADDR:7001 \
     -addr $IP_ADDR:4001 \
     -name etcdindocker1234
