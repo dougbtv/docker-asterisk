14a590e57a88f0abc611a21b7d851cc65a723fed
dougbtv <douglaskippsmith@gmail.com>
2016-05-15 15:07:58 -0400

    [test] updates wait for asterisk to be asterisk13 compatible

--- test/wait_for_asterisk.sh
+++ test/wait_for_asterisk.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 while /bin/true; do
-	logs=`docker logs $1`
-	if [[ $logs =~ "Asterisk Ready" ]]; then
-	  echo "$1 asterisk is ready."
+    logs=`docker logs $1`
+    if [[ $logs =~ "Asterisk Ready" ]]; then
+      echo "$1 asterisk is ready."
       break;
     else
-      echo "waiting..."
-      sleep 1
+      if [[ $logs =~ "pjlib 2.4.5 for POSIX initialized" ]]; then
+        echo "$1 asterisk is ready."
+        break;
+      else          
+        echo "waiting..."
+        sleep 1
+      fi
     fi
 done
