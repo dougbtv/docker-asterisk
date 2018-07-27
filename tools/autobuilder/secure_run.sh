6fdad9f9e1e080ca850df237fe8bc58ef98edd93
dougbtv <dosmith@redhat.com>
2018-07-27 09:20:37 -0400

    [delete] Remove autobuilder dir

--- tools/autobuilder/secure_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-node autobuilder.js -u asteriskautobuilder -p "iErs6lw6yJwugQe01n3XrO7BLgl4sQ5V" \
-  --docker_user dougbtv --docker_password "SWgGzf@bZh@nc1g30i^98AV4MXkBNg$3" --docker_email "douglaskippsmith@gmail.com" \
-  --irc_debug --irc_disabled
